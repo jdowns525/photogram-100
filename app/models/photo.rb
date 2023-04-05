653ae4bcd20a532f5d4b597ca47bfa7d2ab02c7d
jdowns525 <83981939+jdowns525@users.noreply.github.com>
2023-04-05 17:12:51 +0000

    commit

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,7 +12,15 @@
 #  owner_id       :integer
 #
 class Photo < ApplicationRecord
-  has_many(:likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy })
-  has_many(:comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy })
-  has_many(:users, { :class_name => "User", :foreign_key => "likes_count", :dependent => :destroy })
+  mount_uploader :image, ImageUploader
+
+    
+  belongs_to :owner, { :required => true, :class_name => "User", :foreign_key => "owner_id" }
+  
+  has_many  :likes, { :class_name => "Like", :foreign_key => "photo_id", :dependent => :destroy }
+
+  has_many  :comments, { :class_name => "Comment", :foreign_key => "photo_id", :dependent => :destroy }
+
+  has_one  :associated_followrequest, { :through => :owner, :source => :sentfollowrequests }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
 #  id              :integer          not null, primary key
 #  comments_count  :integer
 #  email           :string
+#  image           :string
 #  likes_count     :integer
 #  password_digest :string
 #  private         :boolean
@@ -16,8 +17,16 @@ class User < ApplicationRecord
   validates :email, :uniqueness => { :case_sensitive => false }
   validates :email, :presence => true
   has_secure_password
-  has_many(:comments, { :class_name => "Comment", :foreign_key => "photo_id", :dependent => :destroy })
-  has_many(:likes, { :class_name => "Like", :foreign_key => "photo_id", :dependent => :destroy })
-  has_many(:followrequests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy })
-  belongs_to(:like_count, { :required => true, :class_name => "Photo", :foreign_key => "likes_count" })
+
+
+has_many :likes, { :class_name => "Like", :foreign_key => "fan_id", :dependent => :destroy }
+
+has_many  :comments, { :class_name => "Comment", :foreign_key => "author_id", :dependent => :destroy }
+
+has_many  :photos, { :class_name => "Photo", :foreign_key => "owner_id", :dependent => :destroy }
+
+has_many  :receivedfollowrequests, { :class_name => "FollowRequest", :foreign_key => "recipient_id", :dependent => :destroy }
+
+has_many  :sentfollowrequests, { :class_name => "FollowRequest", :foreign_key => "sender_id", :dependent => :destroy }
 end
+
